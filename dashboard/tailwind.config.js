52763f379200885438780c0449ed47a3494510f1
LuisSempre <luis.silva@viannasempre.com.br>
2022-10-04 21:40:27 -0300

    Create Routes Views

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   presets: [],
   darkMode: "media", // or 'class'
   theme: {
